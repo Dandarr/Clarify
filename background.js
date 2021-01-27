bed9ec9e41ba03142605399fb4a15d69aa69f5a9
Dandarr <thomas.smith.6007@gmail.com>
2021-01-27 15:31:02 +0000

    Tinkering with background.js to see how browser interacts with code

--- background.js
+++ background.js
@@ -1,14 +1,10 @@
-
 const type = new Array('textarea','input','div','h1','h2','h3','span','p','li','a');
 
-chrome.runtime.onMessage.addListener(textChange, init);
-//^^^Uncaught TypeError: This event does not support filters
-chrome.browserAction.setBadgeText({text: 'ON'});
-chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+const selectElement = document.querySelector('.typeDrop');
 
-function textChange(request, sender, sendResponse) {
+selectElement.addEventListener('change', (event) => { 
   let stylePoint  = document.createElement ('style');
-  switch (typeSelection) {
+  switch (selectElement) {
     case 'Comic Neue':
       console.log('changing font to comic neue');
       stylePoint.textContent = WebFont.load({
@@ -21,9 +17,4 @@ function textChange(request, sender, sendResponse) {
       console.log('changing font to arial');
       break;
   }
-}
-
-function init(){
-  //the button should now be working will have to check though
-  typeButton.addEventListener('click', textChange());}
-  let typeSelection = chrome.getElementById(typeSelect).value;
+});
