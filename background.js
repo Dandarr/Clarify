47d75e0a21251b9ce73614940e9fa2453e7e143e
Dandarr <thomas.smith.6007@gmail.com>
2021-02-02 21:50:35 +0000

    re-added some functionality

--- background.js
+++ background.js
@@ -4,9 +4,24 @@ function init() {
     function textChange() {
         console.log("Button clicked");
         alert("It works!");
-    }
+            if (textVal == "0"){
+                console.log('changing font to arial');
+            }
+            else if (textVal == "1"){
+                console.log('changing font to comic neue');
+                stylePoint.textContent = WebFont.load({
+                     google: {families: ['Comic Neue', 'cursive']}});
+            }
+            else if (textVal == "2"){
+                console.log('changing font to dyslexi');
+            }
+        }
+
 
     var submit = document.getElementById('send');
     submit.addEventListener('click', textChange);
     console.log("Loaded");
-}
+
+    var textSel = document.getElementById('typeSelect');
+    var textVal = textSel.value;
+    }
