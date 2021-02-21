23d823af4a01be695fddfd88de84cc4b52364d3d
Dandarr <thomas.smith.6007@gmail.com>
2021-02-21 16:32:37 +0000

    added stylesheet elements and fixed naming discrepancies in code

--- background.js
+++ background.js
@@ -15,25 +15,40 @@ function init() {
         console.log("Button clicked");
             if (textVal == "0"){
                 console.log(textVal);
-                console.log('changing font to roboto');
-                style.textContent = "@font-face { font-family: Byslexic; src: url('" + chrome.extension.getURL ("./fonts/Roboto-Regular.ttf") + "'); }";
+                console.log('changing font to Roboto');
+                style.textContent = "@font-face { font-family: roboto; src: url('" + chrome.extension.getURL ("./fonts/Roboto-Regular.ttf") + "'); }";
                 document.head.appendChild (style);
+                let textDecision = "roboto";
             }
             else if (textVal == "1"){
                 console.log(textVal);
-                console.log('changing font to comic neue');
-                style.textContent = "@font-face { font-family: Byslexic; src: url('" + chrome.extension.getURL ("./fonts/PortLligatSans-Regular.ttf") + "'); }";
+                console.log('changing font to Port Lilgat');
+                style.textContent = "@font-face { font-family: portLilgat; src: url('" + chrome.extension.getURL ("./fonts/PortLligatSans-Regular.ttf") + "'); }";
                 document.head.appendChild (style);
+                let textDecision = "portLilgat";
             }
             else if (textVal == "2"){
                 console.log(textVal);
-                console.log('changing font to abel');
-                style.textContent = "@font-face { font-family: Byslexic; src: url('" + chrome.extension.getURL ("./fonts/Abel-Regular.ttf") + "'); }";
+                console.log('changing font to Abel');
+                style.textContent = "@font-face { font-family: abel; src: url('" + chrome.extension.getURL ("./fonts/Abel-Regular.ttf") + "'); }";
                 document.head.appendChild (style);
+                let textDecision = "abel";
             }
             else{
                 console.log("unable to specify font selection");
             }
+            var addClass = function(){
+                for(var i=0;i<types.length;i++){
+                  var node = document.getElementsByTagName(types[i]);
+                  for(var y=0;y<node.length;y++){
+                    // make sure there is text in the element first
+                    if (node[y].textContent){
+                      // add class to matching elements for font style
+                      node[y].classList.add(textDecision);
+                    }
+                  }
+                }
+              };
         }
 
     var submit = document.getElementById('send');
