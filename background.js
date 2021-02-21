5bf373811c0baec3a028d869697fad81af580996
Dandarr <thomas.smith.6007@gmail.com>
2021-02-21 16:57:18 +0000

    Update background.js

--- background.js
+++ background.js
@@ -13,42 +13,56 @@ function init() {
         var textVal = textSel.value;
         console.log(textVal);
         console.log("Button clicked");
+        
             if (textVal == "0"){
                 console.log(textVal);
                 console.log('changing font to Roboto');
+                //load font
                 style.textContent = "@font-face { font-family: roboto; src: url('" + chrome.extension.getURL ("./fonts/Roboto-Regular.ttf") + "'); }";
                 document.head.appendChild (style);
                 let textDecision = "roboto";
+                // perform the font replace
+                addClass();
             }
+
             else if (textVal == "1"){
                 console.log(textVal);
                 console.log('changing font to Port Lilgat');
+                //load font
                 style.textContent = "@font-face { font-family: portLilgat; src: url('" + chrome.extension.getURL ("./fonts/PortLligatSans-Regular.ttf") + "'); }";
                 document.head.appendChild (style);
                 let textDecision = "portLilgat";
+                // perform the font replace
+                addClass();
             }
+
             else if (textVal == "2"){
                 console.log(textVal);
                 console.log('changing font to Abel');
+                //load font
                 style.textContent = "@font-face { font-family: abel; src: url('" + chrome.extension.getURL ("./fonts/Abel-Regular.ttf") + "'); }";
                 document.head.appendChild (style);
                 let textDecision = "abel";
+                // perform the font replace
+                addClass();
             }
+
             else{
                 console.log("unable to specify font selection");
             }
-            var addClass = function(){
-                for(var i=0;i<types.length;i++){
-                  var node = document.getElementsByTagName(types[i]);
-                  for(var y=0;y<node.length;y++){
-                    // make sure there is text in the element first
-                    if (node[y].textContent){
-                      // add class to matching elements for font style
-                      node[y].classList.add(textDecision);
-                    }
-                  }
+        }
+
+        function addClass(textDecision) {
+            for(var i=0;i<types.length;i++){
+              var node = document.getElementsByTagName(types[i]);
+              for(var y=0;y<node.length;y++){
+                // this make sure that there is text in the element first
+                if (node[y].textContent){
+                  // this adds the class to the matching elements for font style based off textDecision
+                  node[y].classList.add(textDecision);
                 }
-              };
+              }
+            }
         }
 
     var submit = document.getElementById('send');
