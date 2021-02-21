ae508b33cd44b27b93c09e2c79f5dd8d60ce856d
Dandarr <thomas.smith.6007@gmail.com>
2021-02-21 16:02:13 +0000

    appropriate style element will be created on dropdown selection

--- background.js
+++ background.js
@@ -2,28 +2,37 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
     var types = new Array("textarea","input","div","h1","h2","h3","span","p","li","a");    
-    var stylePoint           = document.createElement ("style");
-    document.head.appendChild (stylePoint);
+    var style = document.createElement ("style");
+    // style.type = "text/css";
+    // dont need to add the .type as it is redundant since style only has one type
 
     function textChange() {
+        // textSel is the dropdown menu for fonts
+        //textVal is the currently selected option from that dropdown
         var textSel = document.getElementById('typeSelect');
         var textVal = textSel.value;
         console.log(textVal);
         console.log("Button clicked");
             if (textVal == "0"){
-                console.log('changing font to arial');
+                console.log(textVal);
+                console.log('changing font to roboto');
+                style.textContent = "@font-face { font-family: Byslexic; src: url('" + chrome.extension.getURL ("./fonts/Roboto-Regular.ttf") + "'); }";
+                document.head.appendChild (style);
             }
             else if (textVal == "1"){
+                console.log(textVal);
                 console.log('changing font to comic neue');
-                stylePoint.textContent = WebFont.load({
-                     google: {families: ['Comic Neue', 'cursive']}});
+                style.textContent = "@font-face { font-family: Byslexic; src: url('" + chrome.extension.getURL ("./fonts/PortLligatSans-Regular.ttf") + "'); }";
+                document.head.appendChild (style);
             }
             else if (textVal == "2"){
-                console.log('changing font to dyslexi');
+                console.log(textVal);
+                console.log('changing font to abel');
+                style.textContent = "@font-face { font-family: Byslexic; src: url('" + chrome.extension.getURL ("./fonts/Abel-Regular.ttf") + "'); }";
+                document.head.appendChild (style);
             }
-
             else{
-                console.log("unable to specify font selection")
+                console.log("unable to specify font selection");
             }
         }
 
