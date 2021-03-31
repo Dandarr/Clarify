fff1d12c97cab6bd59645f20039ed241d5b100e1
Dandarr <thomas.smith.6007@gmail.com>
2021-03-31 22:29:31 +0100

    Classes are actually applied to DOM elements now
    CSS is still broken though

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 window.addEventListener('DOMContentLoaded', main);
-window.addEventListener('DOMContentLoaded', changeCSS());
 
 
-var elements = new Array("textarea","input","div","h1","h2","h3","span","p","li","a");
 //CSS file definintley arrives at page
 //CSS changes will work when called
 //text replace definitley parses the correct text input to addClass
@@ -11,7 +9,6 @@ var elements = new Array("textarea","input","div","h1","h2","h3","span","p","li"
 //i'm pretty sure this is working but not sure how to verify it
 // gets the document url of the currently active chrome tab
 function main() {
-    console.log("currently selected elements are: " + elements);
     /*
     //create element style
     var style = currentTab.createElement ("style");
@@ -30,19 +27,19 @@ function main() {
         if (textVal == "0"){
             console.log(textVal);
             console.log('changing font to Roboto');
-            doStuffWithDom('roboto');
-          }
+            sendMessage('roboto');
+        }
 
           else if (textVal == "1"){
               console.log(textVal);
               console.log('changing font to Port Lilgat');
-              modifyDOM('portLilgat');
+              sendMessage('portLilgat');
           }
 
           else if (textVal == "2"){
               console.log(textVal);
               console.log('changing font to Abel');
-              modifyDOM('abel');
+              sendMessage('abel');
           }
 
           else{
@@ -50,54 +47,27 @@ function main() {
               console.log("unable to specify font selection");
           }
     }
-
     //init
     document.getElementById('send').addEventListener('click', textReplace);
     console.log("Loaded Main");
-
-
-/*
-// this should select the currently in use chrome tab and set it to currentTab
-let currentTab;
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
-function(tabs){
-getCurrentURL(tabs[0].url);});
-function getCurrentURL(tab){
-    currentTab = tab;
-}
-
-*/
 }
 
 
-// A function to use as callback
-function doStuffWithDom(domContent, textDecision) {
-    console.log('I received the following DOM content:\n' + domContent);
-    for(var i = 0; i < elements.length; i++){
-        var node = document.getElementsByTagName(elements[i]);
-        for(var y = 0; y < node.length; y++){
-             node[y].classList.add(textDecision);
-             console.log("font changed");
-        }
-    }
+function sendMessage(classy) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			classSelect: classy
+		}, function(response) {
+			// should be the DOM
+			console.log(response);
+		});
+	});
 }
 
-// When the browser-action button is clicked...
-chrome.browserAction.onClicked.addListener(function (tab) {
-        // ...if it matches, send a message specifying a callback too
-    chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+/* Listen for messages */
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	console.log("Message received in background.js: " + request.DOM);
+	sendResponse({
+		response: "Message Received",
+	});
 });
-
-
-
-
-// adds the css page to the target webpage and replaces it
-// this has been verified as working
-function changeCSS(cssLinkIndex) {
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", chrome.runtime.getURL('/style.css'));
-    document.getElementsByTagName("head").item(cssLinkIndex).appendChild(newlink);
-    console.log("linked stylesheet");
-}
--- content.js
+++ content.js
@@ -1,8 +1,22 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
+window.onload = function() {
+    console.log("Starting page");
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", chrome.runtime.getURL('style.css'));
+    // var stylesheet = chrome.extension.getURL("style.css");
+    document.getElementsByTagName("head")[0].appendChild(newlink);
+}
+
+/* Listen for messages */
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    console.log("Message received");
+    var elements = document.getElementsByTagName("*");
+    console.log(elements);
+    for(var node of elements){
+        console.log(node);
+        node.classList.add(msg.classSelect);
     }
+    document.documentElement.classList.add(msg.classSelect);
+    sendResponse({});
 });
