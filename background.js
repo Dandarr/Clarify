7602119aac5944bff84b4a455866a7c07662be74
Dandarr <thomas.smith.6007@gmail.com>
2021-05-18 22:32:59 +0100

    cleaned out uneeded comments and fixed sizing bug (at last!)

--- background.js
+++ background.js
@@ -1,21 +1,8 @@
-/*
-Ok so basically the way this works is:
- - main is initialised on DOM content of the popup loading
- - When submit button is hit it fires all the functions in main
- - each function makes a decision on what class it should send based on it's input method:
-    - text replaced based on if statment for dropdown menu
-    - size and spacing is figured from a slider which indicates an array value that then constructs the name of the correct class
-    - colour scheme is based off of radio buttons and parses that value onwards
-- these classnames then get parsed to the sendMessage function which parses the information to the content script where the target webpages DOM has been selected. 
-- this then applies the classes to every element and each of its nodes in the target DOM.
-*/
-
-
 //init main
 window.addEventListener('DOMContentLoaded', main);
 
 //initialises the arrays for text spacing and text sizes functions
-const textSizes = [8, 10, 12, 16, 24, 32];
+const textSizes = ['8', '10', '12', '16', '24', '32'];
 const textSpacings = ['tight', 'normal', 'wide', 'wider'];
 
 function main() {
@@ -53,7 +40,8 @@ function main() {
     //configures text size based off of a slider
     function sizeConfig(size){
         //size is going wrong here
-        console.log(typeof size);
+        //it's litterally the same kind of object as spacing on 53
+        console.log(size);
         let sizeDecision = textSizes[size];
         let message = 'ts'+ sizeDecision;
         console.log('the text size selected was: ' + message);
@@ -62,6 +50,7 @@ function main() {
 
     //configures the spacing between letters based on a slider
     function spacingConfig(spacing){
+        console.log(spacing);
         let spaceDecision = textSpacings[spacing];
         let message = 'ts'+ spaceDecision;
         console.log('the text size selected was: ' + message);
@@ -88,8 +77,8 @@ function main() {
      sizeElement.addEventListener('change', (event) => {
          const sizeResult = document.getElementById("sizeDisplay");
          sizeResult.textContent = textSizes[event.target.value] + 'px';
-         let sizeX = sizeElement.value;
-         spacingConfig(sizeX);
+         let size = event.target.value;
+         sizeConfig(size);
      });
      //initilasises the displays for spacing sliders and parses info to SpacingConfig
      const spacingElement = document.getElementById('spacing');
